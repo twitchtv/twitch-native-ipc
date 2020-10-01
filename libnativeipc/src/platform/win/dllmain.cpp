f3284d628a1948346e2315309810a725eefc13f9
Feelsjelloman <ross.gardner@live.com>
2020-09-30 17:39:44 -0700

    Update dllmain.cpp

--- libnativeipc/src/platform/win/dllmain.cpp
+++ libnativeipc/src/platform/win/dllmain.cpp
@@ -1,3 +1,6 @@
+// Copyright Twitch Interactive, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include <Windows.h>
 
